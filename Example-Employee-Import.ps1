8e42a5a99e7be698d2f14cc55d644000533e96ae
Gerry (Gary) Berg <52010354+gerryberg@users.noreply.github.com>
2021-06-16 08:33:04 -0400

    Update Example-Employee-Import.ps1

--- Example-Employee-Import.ps1
+++ Example-Employee-Import.ps1
@@ -1,3 +1,4 @@
 $Uri = '[PASTE Batch Data Import Url HERE]'
 $FilePath = 'Example-EmployeeManagement-Employee-data.csv'
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 $upload= Invoke-WebRequest -Uri $Uri -Method Post -InFile $FilePath -ContentType 'application/x-binary'
